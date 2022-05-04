e5736234bb8872fa7d38428caf22691ccd65240e
askelio <gogolyaddza@gmail.com>
2022-05-05 02:02:46 +0300

    Setup was added

---
+++ SerializerFactory/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Serializer',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Alex Hryharovich',
+      author_email='gogolyadza@gmail.com',
+      packages=['SerializerFactory', 'SerializerFactory.Serializers'],
+     )
