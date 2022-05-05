136d25fe9e5b0c4cdd0b0150c3c5865805317144
askelio <gogolyaddza@gmail.com>
2022-05-06 00:46:01 +0300

    temp commit

--- serializer_utility/setup.py
+++ serializer_utility/setup.py
@@ -5,7 +5,7 @@ setup(name='Serializer',
       description='Python Distribution Utilities',
       author='Alex Hryharovich',
       author_email='gogolyadza@gmail.com',
-      packages=['SerializerFactory', 'SerializerFactory.Serializers', 'ConsoleUtility', 'ConsoleUtility.Files','Tests'],
+      packages=['SerializerFactory', 'SerializerFactory.Serializers', 'ConsoleUtility', 'ConsoleUtility.Files', 'Tests'],
       install_requires=['PyYaml == 6.0',
                         'PyToml == 0.1.21'],
       entry_points={'console_scripts': ['utility = ConsoleUtility.utility:utility_code']}
